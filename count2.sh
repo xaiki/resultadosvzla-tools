02acac9d57579c47dbf8a28208578a2b2aa1ef58
Niv Sardi <xaiki@evilgiggle.com>
2024-08-01 10:18:55 -0300

    fixups by mis2centavos
    update readme: fix bugs and typos
    "--set *.enable=0 --set qrcode.enable=1" hace que solo busque QR, para evitar errores y mejorar la performance.
    Y además cuenta los votos de todos los candidatos (para tener bien los porcentajes después)

--- count2.sh
+++ count2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "record,maduro,gonzalez"
+echo "Acta,Maduro,Martinez,Bertucci,Brito,Ecarri,Fermin,Ceballos,Gonzalez,Marquez,Rausseo"
 ls $1 | grep '.jpg' | xargs --max-procs=32 -n1 sh barimg.sh $1

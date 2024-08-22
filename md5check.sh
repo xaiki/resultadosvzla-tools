5e48f8f6d1cd4c7186aa5ca185d656d065fbf318
Marco D'Agostini <madacol10@gmail.com>
2024-08-22 14:43:56 +0200

    mostrar input de md5sum cuando está mal formado

--- md5check.sh
+++ md5check.sh
@@ -2,5 +2,5 @@
 
 for filename in $@; do
   etag=$(curl -I "https://static.resultadosconvzla.com/$filename" | grep etag | sed -E 's/.*?([0-9a-f]{32}).*/\1/')
-  echo "$etag $filename" | md5sum -c
+  echo "$etag $filepath" | md5sum -c || echo "$etag - $filepath"
 done
